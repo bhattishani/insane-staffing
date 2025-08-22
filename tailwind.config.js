ca5afc40c4b7ee0280a4fd669af7d9bd5b9e7738
bhatti__shani <bhattishan090@gmail.com>
2025-08-23 01:26:44 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./*.html",
-    "./assets/**/*.{js,jsx,ts,tsx,vue}"
-  ],
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        'dark': {
-          500: '#1f2937',
-          600: '#1f2937',
-          700: '#1f2937'
-        }
-      }
-    }
+    extend: {},
   },
   plugins: [],
 }
+
